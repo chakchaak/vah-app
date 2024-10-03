5bccdb992e4f2746de0d089007194d829ec3c965
Студенческая <@rbiu.local>
2024-10-03 12:56:58 +0500

    work out read with pagination and delete

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.main')
 @section('content')
+<div class="md:container md:mx-auto">
   <h1>список студентов</h1>
-  @foreach($students as $student)
-    <p>{{ $student->lname }}</p>
-  @endforeach
+    @foreach($students as $student)
+    <div class="flex justify-between">
+      <img class="size-16" src="{{$student->path_img}}" alt="{{$student->lname}}">
+      <p>{{$student->lname}}</p>
+      <p>{{$student->fname}}</p>
+      <p>{{$student->age}}</p>
+      <div>
+        <form action="{{ route('student.destroy', $student->id) }}" method="POST">
+          @csrf
+          @method('delete')
+          <button type="submit">Удалить</button>
+        </form>
+      </div>
+    </div>
+      <p>{{ $student->lname }}</p>
+    @endforeach
+    {{ $students->links() }}
+</div>
 @endsection()
